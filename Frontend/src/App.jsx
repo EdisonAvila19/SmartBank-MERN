bb3049f0b35afd83f6ea7d28f620869b685da525
edison <edison_20_04@hotmail.com>
2024-08-31 13:25:43 -0500

    Account route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { AuthProvider } from './context/AuthContext'
 import Header from './components/Header'
 import NavBar from './components/NavBar'
 import ProtectedRoute from './helpers/ProtectedRoute'
+import AccountRoute from './helpers/AccountRoute'
 
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
@@ -12,7 +13,6 @@ import Transactions from './pages/Transactions'
 import NewAccount from './pages/NewAccount'
 import NewTransaction from './pages/NewTransaction'
 import NotFound from './pages/404'
-
 
 function App() {
 
@@ -23,11 +23,13 @@ function App() {
         <Routes>
           <Route path="/" element={ <Home /> } />
           <Route element={ <ProtectedRoute /> }>
-            <Route element={ <NavBar /> } >
-              <Route path="/dashboard" element={ <Dashboard /> } />
-              <Route path='/transactions/:id' element={ <Transactions /> } />
-              <Route path='/new-account' element={ <NewAccount /> } />
-              <Route path='/new-transaction' element={ <NewTransaction /> } />
+            <Route element={ <AccountRoute /> }>
+              <Route element={ <NavBar /> } >
+                <Route path="/dashboard" element={ <Dashboard /> } />
+                <Route path='/transactions/:id' element={ <Transactions /> } />
+                <Route path='/new-account' element={ <NewAccount /> } />
+                <Route path='/new-transaction' element={ <NewTransaction /> } />
+              </Route>
             </Route>
           </Route>
           <Route path='*' element={<NotFound />} />
